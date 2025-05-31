0bfa76f82a36b56d6f8496ea9d2fb7fb2b993b2e
juanhts04 <juancarlossintor@gmail.com>
2025-05-31 12:21:26 -0500

    maicol se lo metio a michel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'registro', component: RegisterComponent },
-  
+
+
 
 
 ];
