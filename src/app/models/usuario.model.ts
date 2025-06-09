3b1efda141e1d9ed6094b2929ac5de6f4a7802f6
juanhts04 <juancarlossintor@gmail.com>
2025-06-08 19:35:29 -0500

    services de  usuarios y modelos

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,8 @@
+
+export interface Usuario {
+  id: number;
+  username: string;
+  password: string;
+  email: string;
+  idjuego: number;
+}
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
 
-  constructor() { }
+  usuariosURL = 'http://localhost:3000/usuarios'
+
+  constructor( private http: HttpClient) { }
+
+   getUsuarios(){
+     return this.http.get(this.usuariosURL);
+   }
+
+   getUsuario(id: number): Observable<any>{
+     return this.http.get(`${this.usuariosURL}/${id}`);
+   }
+
 }
